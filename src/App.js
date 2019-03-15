b0a46970d2beff383b14b35a7f969a11e9035ff9
David Mangold <pinderby@gmail.com>
2019-03-15 00:21:47 -0700

    Basic implementation of relay with fragment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import RelayEnvironment from "./RelayEnvironment.js";
+import graphql from "babel-plugin-relay/macro";
+import { QueryRenderer } from "react-relay";
+import TestChild from './TestChild';
 import logo from './logo.svg';
 import './App.css';
 
@@ -19,6 +23,49 @@ class App extends Component {
           >
             Learn React
           </a>
+          {/* <QueryRenderer
+            environment={RelayEnvironment}
+            query={graphql`
+              query AppQuery {
+                version
+              }
+            `}
+            variables={{}}
+            render={({error, props}) => {
+              if (error) {
+                return <div>Error!</div>;
+              }
+              if (!props) {
+                return <div>Loading...</div>;
+              }
+              return <div>Version: {props.version}</div>;
+            }}
+          /> */}
+          <QueryRenderer
+            environment={RelayEnvironment}
+            query={graphql`
+              query AppQuery {
+                repo(username: "gweeks", reponame: "IMDB") {
+                  id
+                  ...TestChild_repo
+                }
+              }
+            `}
+            variables={{}}
+            render={({error, props}) => {
+              if (error) {
+                return <div>Error!</div>;
+              }
+              if (!props) {
+                return <div>Loading...</div>;
+              }
+              console.log("props: ", props);
+              return <div>
+                      id: {props.repo.id}
+                      <TestChild repo={props.repo} component="TestChild" />
+                    </div>;
+            }}
+          />
         </header>
       </div>
     );
